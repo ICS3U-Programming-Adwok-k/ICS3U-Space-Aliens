2d814ca3907feb55b05be4499726cb7ad69b9684
Adwok Adiebo <adwok.adiebo@stu.ocsb.ca>
2025-05-30 17:05:43 +0000

    code

--- code.py
+++ code.py
@@ -8,16 +8,45 @@ def game_scene():
     # Import the image and assign to a variable
     image_bank_background = stage.Bank.from_bmp16(
         "space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+
     background = stage.Grid(image_bank_background, 10, 8)
+
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
 
     game = stage.Stage(ugame.display, 60)
     # Our image is going to be refreshed 60 times
-    game.layers = [background]
+    game.layers = [ship] + [background]
+
     game.render_block()
 
+    # repeat forever, game loop
     while True:
-        # This repeats forever
-        pass
+        # get user input
+        keys = ugame.buttons.get_pressed()
+
+        if keys & ugame.K_X:
+            print("A")
+        if keys & ugame.K_O:
+            print("B")
+        if keys & ugame.K_START:
+            print("Start")
+        if keys & ugame.K_SELECT:
+            print("Select")
+        if keys & ugame.K_RIGHT:
+            ship.move(ship.x + 1, ship.y)
+        if keys & ugame.K_LEFT:
+            ship.move(ship.x - 1, ship.y)
+        if keys & ugame.K_UP:
+            ship.move(ship.x, ship.y - 1)
+        if keys & ugame.K_DOWN:
+            ship.move(ship.x, ship.y + 1)
+        # update game logic
+
+        # redraw Sprites
+
+        game.render_sprites([ship])
+        game.tick()
 
 
 if __name__ == "__main__":
